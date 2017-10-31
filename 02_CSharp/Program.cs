56a052091d7c4e96d97b29654500acbac238635e
Bartek <vulkan44@wp.pl>
2017-10-31 21:22:54 +0100

    projects init

---
+++ 02_CSharp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*------------------------------CASE 1---------------------------*/
+            int x = 5;
+
+            if (x == 5)
+            {
+                Console.WriteLine("x = 5");
+            }
+
+            Console.ReadLine();
+
+
+
+            /*------------------------------CASE 2---------------------------*/
+            //int x = 5;
+            //int y=11;
+            //if (x == 5)
+            //{
+            //    Console.WriteLine("x = 5");
+
+            //    if (y >= 10)
+            //    {
+            //        Console.WriteLine("Second case is also fulfilled");
+            //    }
+            //}
+
+
+
+            /*------------------------------CASE 3---------------------------*/
+            //int x = 5;
+            //int y = 11;
+            //if (x == 5 && y >10)
+            //{
+            //    Console.WriteLine("x = 5");
+            //}
+
+
+
+            /*------------------------------CASE 4---------------------------*/
+            //// The condition checks if it is after 18 o'clock
+            //if (DateTime.Now.Hour >= 18)
+            //{
+            //    Console.WriteLine("Good evening my friend!");
+            //}
+            //// The condition checks if it is before 18 o'clock
+            //else
+            //{
+            //    Console.WriteLine("Good morning my friend!");
+            //}
+
+
+
+            /*------------------------------CASE 5---------------------------*/
+        }
+    }
+}
